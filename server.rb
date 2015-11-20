72bc6218f813a7354a0dfbe31e5345ef7fe5b975
nitanilla <nitanilla@gmail.com>
2015-11-20 18:05:55 -0200

    adding CORS

--- server.rb
+++ server.rb
@@ -4,5 +4,6 @@ require "json"
 
 get "/downloads.json" do
   headers["Content-Type"] = "application/json"
+  headers["Access-Control-Allow-Origin"] = "http://wtrans.inf.puc-rio.br*" 
   JSON.unparse(AnalyticsClient.get_downloads)
 end
