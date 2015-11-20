f790f37ab271d87ba0873c8c031f3ca226bff1f5
nitanilla <nitanilla@gmail.com>
2015-11-20 18:08:16 -0200

    using cors with environment variable

--- server.rb
+++ server.rb
@@ -4,6 +4,6 @@ require "json"
 
 get "/downloads.json" do
   headers["Content-Type"] = "application/json"
-  headers["Access-Control-Allow-Origin"] = "http://wtrans.inf.puc-rio.br*" 
+  headers["Access-Control-Allow-Origin"] = ENV["CORS_DOMAIN"]
   JSON.unparse(AnalyticsClient.get_downloads)
 end
